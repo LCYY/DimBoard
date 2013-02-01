af5b6f6eed3a050b210dcb1724edd90407d2daac
conicacui <conica.c@gmail.com>
2013-02-01 16:44:48 +0800

    Changed app icon. Add OveralInfoViewController.

---
+++ DimBoard/DimBoard/OveralInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  OveralInfoViewController.h
+//  DimBoard
+//
+//  Created by conicacui on 1/2/13.
+//  Copyright (c) 2013 LCYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OveralInfoViewController : UIViewController
+
+@end
